00f0cbcd9bcea969b1162d88f099a2969b3682a0
Jose Molina <mol14492@uvg.edu.gt>
2018-03-28 16:15:43 -0600

    Add files via upload

---
+++ .gitignore/test_plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+valor1=input("Ingrese un valor:")
+valor2=input("Ingrese un valor:")
+valor3=input("Ingrese un valor:")
+valor4=input("Ingrese un valor:")
+
+plt.plot([valor1,valor2, valor3, valor4])
+plt.ylabel('some numbers')
+plt.show()
+
