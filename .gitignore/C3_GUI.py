d01a1fa1aa4e8f14ac5ed6ffdbd0203699b9ff71
Jose Molina <mol14492@uvg.edu.gt>
2018-03-31 18:38:34 -0600

    Update C3_GUI.py

--- .gitignore/C3_GUI.py
+++ .gitignore/C3_GUI.py
@@ -2,13 +2,47 @@ from Tkinter import *
 
 root = Tk()
 root.title("Circuitos 3")
-root.geometry('600x175+600+600')
+root.geometry('400x400')
 
 
-photo1= PhotoImage(file="logo.jpg")
+## FUNCIONES ##
+
+def dato_recibido():
+	d_temp = d_enviar.get()
+	d_display=Text(master=root, height=2, width=4)
+	d_display.grid(column=1, row=4)
+	d_display.insert(INSERT,d_temp)
+
+
+## LABELS ##
+## Titulo ##
+title = Label(text="Control Motor",font='Helvetica 18 bold', fg="blue")
+title.grid(column = 1, row=0)
+## Label para enviar ##
+l_enviar = Label(text="Escribe el caracter:")
+l_enviar.grid(column=0, row=1)
+
+l_recibir = Label(text="Recibiendo datos:")
+l_recibir.grid(column=0, row=4)
+
+## Botones ##
+## boton para enviar ##
+b_enviar = Button(text="ENVIAR", fg="green", command = dato_recibido, width="10")
+b_enviar.grid(column=0, row=2)
+
+
+## Entradas ##
+d_enviar = Entry()
+d_enviar.grid(column=1, row=1)
+
 
 
 root.mainloop()
+
+
+
+
+
 
 
 
